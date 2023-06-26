14e6ba9bbc03df0e875591030c1a91756eed523b
Ajay Shanker <ajayshanker1998@gmail.com>
2023-06-26 20:37:03 +0800

    Initial Commit

---
+++ src/secrets.h
@@ -0,0 +1,15 @@
+
+
+// #define SECRET
+#define THINGNAME "ESP32CAM_0"
+
+const char WIFI_SSID[] = "GreenCrateSG";
+const char WIFI_PASSWORD[] = "GreenCrateSG";
+
+// #define SECRET_SSID ""
+// #define SECRET_PASS ""
+
+#define MQTT_SERVER "192.168.8.103"
+#define MQTT_PORT 1883
+#define MQTTUSER "GreenCrateSG"
+#define MQTTPASSWORD "GreenCrateSG"
